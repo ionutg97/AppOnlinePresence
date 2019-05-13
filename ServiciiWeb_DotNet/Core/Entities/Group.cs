c7d92da821bffad0b2ee65aa6bf13ec4ad00f0bb
theona09 <theona_09@yahoo.com>
2019-05-13 23:06:28 +0300

    servlet and ignore

---
+++ ServiciiWeb_DotNet/Core/Entities/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Group
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Student> Students { get; set; }
+    }
+}
