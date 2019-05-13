e446f565f9a10f12dbea307d185907e72cf65111
infinity1997 <ionut_and97@yahoo.com>
2019-05-13 22:49:50 +0300

    Solved gitignore

---
+++ ServiciiWeb_DotNet/WebApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using WebApplication1;
+
+namespace AttendanceApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
